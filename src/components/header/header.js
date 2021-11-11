edfc073674b548f136f22ff9a1499ff2a655e19e
taegorov <taegorov@gmail.com>
2021-11-10 20:12:24 -0800

    updates Nile font, updates Filter By font, moves some things to JSS

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { Typography, Toolbar, IconButton, Grid, Button } from '@material-ui/core';
+import { Typography, Toolbar, IconButton, Grid, Button, makeStyles } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 // import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import ShoppingCartTwoToneIcon from '@material-ui/icons/ShoppingCartTwoTone';
@@ -11,9 +11,17 @@ import { StylesProvider } from '@material-ui/styles';
 import './header.css';
 
 
+const useStyles = makeStyles({
+  text: {
+    fontFamily: 'Ceviche One',
+    fontSize: '8em',
+  },
+})
+
+
 
 function Header(props) {
-
+  const header = useStyles();
   return (
     <div id="header">
       {/* <AppBar position="static"> */}
@@ -23,7 +31,7 @@ function Header(props) {
           <IconButton edge="start" aria-label="menu">
             <MenuIcon id="nav-button" />
           </IconButton>
-          <Typography variant="h3"> Nile </Typography>
+          <Typography className={header.text} variant="h3"> Nile </Typography>
           <Grid className={StylesProvider.button}>
             <Button align-content="right" id="nav-button">
               <ShoppingCartTwoToneIcon fontSize="small" />
