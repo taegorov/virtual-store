3ccb49bb5f568909390ef2ba1c2c4a0de2f6bf40
taegorov <taegorov@gmail.com>
2022-01-19 17:29:15 -0800

    refactors and removes unecessary reducer. adds category description when filtering categories and styles accordingly

--- src/store/categories.js
+++ src/store/categories.js
@@ -1,18 +1,17 @@
 const initialState = {
   categoryList: [
-    { _id: 1, name: 'All', description: 'All Products' },
-    { _id: 10, name: 'Accessibility', description: 'Category Description Goes Here' },
-    { _id: 20, name: 'Web Design', description: 'Category Description Goes Here' },
-    { _id: 30, name: 'Design', description: 'Category Description Goes Here' },
-    { _id: 40, name: 'Apps', description: 'Category Description Goes Here' },
-    { _id: 60, name: 'Tutoring', description: 'Category Description Goes Here' },
-    { _id: 60, name: 'Misc', description: 'Category Description Goes Here' },
-    { _id: 70, name: 'Your Services', description: 'Category Description Goes Here' }
-
+    { _id: 1, name: 'All', description: 'All products' },
+    { _id: 10, name: 'Accessibility', description: 'Accessibility improvements such as text-to-speech and color-blind friendliness' },
+    { _id: 20, name: 'Web Design', description: 'Design services specifically for your website' },
+    { _id: 30, name: 'Design', description: 'General design services such as logo and brand book creation' },
+    { _id: 40, name: 'Apps', description: 'App creation and assistance' },
+    { _id: 60, name: 'Tutoring', description: 'Hands-on teaching of various design- and coding-related services' },
+    { _id: 60, name: 'Misc', description: 'Everything else' },
+    { _id: 70, name: 'Your Services', description: 'Your services and your services only' }
   ],
 
   activatedCategory: '',
-  activatedDescription: '',
+  activatedDescription: 'All products',
 }
 
 
@@ -22,12 +21,11 @@ export default function reducer(state = initialState, action) {
   switch (type) {
     case 'INACTIVE':
       return initialState;
+    // return { ...state, activatedCategory: payload.category, activatedDescription: initialState.categoryList.find(item => item.name === payload.category).description }
     case 'ACTIVE':
-      return { ...state, activatedCategory: payload.category, activatedDescription: payload.description }
+      return { ...state, activatedCategory: payload.category, activatedDescription: initialState.categoryList.find(item => item.name === payload.category).description }
     case 'ALLPRODUCTS':
-      // return initialState;
-      // return { payload }
-      return { payload, activatedDescription: payload.description }
+      return { payload, activatedDescription: initialState.categoryList.find(item => item.name === payload.category).description }
     default:
       return state;
   }
@@ -43,16 +41,6 @@ export const inactive = () => {
 export const active = (category, description) => {
   return {
     type: "ACTIVE",
-    payload: {
-      category,
-      description
-    }
-  }
-}
-
-export const allProducts = (category, description) => {
-  return {
-    type: "ALLPRODUCTS",
     payload: {
       category,
       description
