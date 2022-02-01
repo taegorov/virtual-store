5087e6134fc8c8d5fcfd5f51e9571d4e868765d7
taegorov <taegorov@gmail.com>
2022-01-31 16:19:03 -0800

    fixes Categories error message, adds ENGINE to packageJSON to fix deploy error

--- src/components/snackbars/Snack.js
+++ src/components/snackbars/Snack.js
@@ -8,60 +8,60 @@ import { openSnackbar, closeSnackbar } from '../../store/misc'
 
 
 function Snack({ snack, openSnackbar, closeSnackbar }) {
-    const { open, severity, message } = snack
-    // const history = useHistory();
+  const { open, severity, message } = snack
+  // const history = useHistory();
 
-    // === snackbar behavior from MUI docs === //
-    // const [openLogout, setOpenLogout] = useState(false);
-
-
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        closeSnackbar();
-    };
-
-    const closeButton = (
-        <>
-            <Button
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={handleClose}
-            >
-                <CloseIcon />
-            </Button>
-        </>
-    );
+  // === snackbar behavior from MUI docs === //
+  // const [openLogout, setOpenLogout] = useState(false);
 
 
-    return (
-        <Snackbar
-            open={open}
-            autoHideDuration={4000}
-            onClose={handleClose}
-        >
-            <MuiAlert
-                action={closeButton}
-                severity={severity}
-            >
-                {message}
-            </MuiAlert>
-        </Snackbar>
-    )
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    closeSnackbar();
+  };
+
+  const closeButton = (
+    <>
+      <Button
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon />
+      </Button>
+    </>
+  );
+
+
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={4000}
+      onClose={handleClose}
+    >
+      <MuiAlert
+        action={closeButton}
+        severity={severity}
+      >
+        {message}
+      </MuiAlert>
+    </Snackbar>
+  )
 }
 
 const mapStateToProps = (state) => {
-    console.log('state is: ', state)
-    return {
-        snack: state.misc.snack
-    }
+  console.log('state is: ', state)
+  return {
+    snack: state.misc.snack
+  }
 }
 
 const mapDispatchToProps = {
-    openSnackbar,
-    closeSnackbar
+  openSnackbar,
+  closeSnackbar
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Snack);
